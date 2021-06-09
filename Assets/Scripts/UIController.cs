ca4d697fc980567c2c8e88176ef11764543b2499
Evly <42288605+Evly@users.noreply.github.com>
2021-06-09 18:32:44 -0500

    UI
    Configuración de UI, script para porcentaje de vida y puntos

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public  Text textoPuntos;
+
+    public  Text textoVida;
+    void Start()
+    {
+        textoPuntos.text = "Puntos: "+GameEvents.getScore().ToString();
+        textoVida.text = "Vida: "+GameEvents.getVida().ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textoPuntos.text = "Puntos: "+GameEvents.getScore().ToString();
+        textoVida.text = "Vida: "+GameEvents.getVida().ToString();
+    }
+}
